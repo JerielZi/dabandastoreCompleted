df5cbef151d95bef2be92b66b6a385ac3e268492
JerielZi <jeriel_zinga@hotmail.com>
2020-05-22 23:46:48 +0100

    Working on page forgot password

--- src/app/validators/session.js
+++ src/app/validators/session.js
@@ -23,7 +23,26 @@ async function login(req, res, next) {
   next()
 }
 
+async function forgot(req, res, next) {
+  const { email } = req.body
+
+  try {
+    let user = await User.findOne({ where: { email } })
+
+    if (!user) return res.render("session/forgot-password", {
+      user:req.body,
+      error: "Email não registado!"
+    })
+
+    next()
+  }catch(err) {
+    console.error(err)
+  }
+
+  next()
+}
 
 module.exports = {
-  login
+  login,
+  forgot
 }
